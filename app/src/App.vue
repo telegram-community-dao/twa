db5cf45fd57c10c56291fc38b648517db221633c
Aler Denisov <aler.zampillo@gmail.com>
2024-01-29 10:10:17 +0300

    feat: update webhook logic

--- app/src/App.vue
+++ app/src/App.vue
@@ -23,26 +23,24 @@ const formState = inject(FORM_STATE_TOKEN, null);
 
 watch(router.currentRoute, (value, previous) => {
   const visited = JSON.parse(localStorage.getItem("visited_pages") ?? "[]");
-  if (visited.includes(value.fullPath)) {
-    return;
-  }
-
-  visited.push(value.fullPath);
-  localStorage.setItem("visited_pages", JSON.stringify(Array.from(new Set(visited))));
   const levels: Partial<Record<string, (route: RouteLocationNormalizedLoaded) => number>> = {
     '/': (route: RouteLocationNormalizedLoaded) => Number(route.query.page || 0),
     '/lp': () => 7,
     '/select': () => 6
   }
   const level = levels[value.path]?.(value) || 0
+  visited.push(value.fullPath);
+  localStorage.setItem("visited_pages", JSON.stringify(Array.from(new Set(visited))));
 
-  if (level > 0) {
+  if (!visited.includes(value.fullPath) && level > 0) {
     alertsService.closeLast()
     alertsService.show(earnTranslatedTemplate.value.replace("{amount}", "100").replace("{balance}", (level * 100).toString()), {
       type: 'success',
       autoClose: 3000
     })
+
   }
+
   if (import.meta.env.VITE_WEBHOOK_URL) {
     fetch(
       import.meta.env.VITE_WEBHOOK_URL + '/sync',
